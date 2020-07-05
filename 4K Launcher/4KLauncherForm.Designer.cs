1b761bfd804526912affe41385c6941d36544815
Alvin4KHD <50411839+Alvin4KHD@users.noreply.github.com>
2020-07-05 16:03:23 +0200

    Initial commit

---
+++ 4K Launcher/4KLauncherForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace _4K_Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.launch_beans = new System.Windows.Forms.Button();
+            this.update_beans = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // launch_beans
+            // 
+            this.launch_beans.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.launch_beans, "launch_beans");
+            this.launch_beans.Name = "launch_beans";
+            this.launch_beans.UseVisualStyleBackColor = true;
+            this.launch_beans.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // update_beans
+            // 
+            this.update_beans.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.update_beans, "update_beans");
+            this.update_beans.Name = "update_beans";
+            this.update_beans.UseVisualStyleBackColor = true;
+            this.update_beans.Click += new System.EventHandler(this.update_beans_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.update_beans);
+            this.Controls.Add(this.launch_beans);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button launch_beans;
+        private System.Windows.Forms.Button update_beans;
+    }
+}
+
---
+++ 4K Launcher/4KLauncherForm.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace _4K_Launcher
+{
+
+    public partial class Form1 : Form
+    {
+
+        string downloadurl = ("https://www.dropbox.com/sh/klyttakbdpn1pac/AAD1yJtHkEkegmA0jNFcx9JZa?dl=1"); // download url, requries a forced download
+        string zipfile = @".\downloading\update.zip"; // this is also a Path where the file saves but it only saves at the 4K Launcher rn 
+        string serverversionurl = ""; // use pastebin, type the current number as "001" not "0.0.1"
+        string clientversionpath = @"..\game\version.txt"; // same with the serverversionurl string but use a txt file instead
+        string gamepath = @"..\game";
+        string gameexecutable = @"..\game\game.exe"; // it can be also a steamid launch, a lnk file or whatever.
+        string gamename = "game";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) 
+        {
+            // TODO: Make a shared code function or smth from update_beans_Click
+        }
+
+        private void beans_logo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(gameexecutable);
+                Application.Exit();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Can't find " + gamename, "Launch Error",
+                  MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+         private void update_beans_Click(object sender, EventArgs e)
+         {
+             WebClient client = new WebClient();
+             int server = int.Parse(client.DownloadString(serverversionurl));
+             int clientversion = int.Parse(File.ReadAllText(clientversionpath));
+             if (server > clientversion)
+             {
+                 client.DownloadFile(downloadurl, zipfile);
+                 try
+                 {
+
+                     {
+                         if (File.Exists(@".downloading\update.zip")) ;
+                            String ZipPath = zipfile;
+                            String extractPath = gamepath;
+                            ZipFile.ExtractToDirectory(ZipPath, extractPath);
+                        if (MessageBox.Show(gamename + " has been successfully updated", "4K Game Launcher", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
+                        {
+                            File.Delete(@".downloading\update.zip");
+                        }
+
+                    }
+
+                 }
+                 catch (Exception)
+                 {
+                     MessageBox.Show("Unable to download update or extract Zip file", "4K Game Launcher",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+                 }
+
+             }
+             else
+             {
+                 MessageBox.Show(gamename + "is already up-to-date", "4K Game Launcher",
+                      MessageBoxButtons.OK, MessageBoxIcon.Information);
+             } 
+    }
+}
+}
