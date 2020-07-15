0513cd84f6304adb32aa0f81c72a07a0281d62a0
Alvin <50411839+Alvin4KHD@users.noreply.github.com>
2020-07-15 14:35:51 +0200

    Update 4KLauncherForm.cs

--- 4K Launcher/4KLauncherForm.cs
+++ 4K Launcher/4KLauncherForm.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
-using System.IO.Compression.ZipFile;
 using System.Net;
 using System.Windows.Forms;
 
@@ -12,20 +11,20 @@ namespace _4K_Launcher
     public partial class Form1 : Form
     {
 
-        string DownloadURL = (""); // download url, requries a forced download
-        string ZipFile = @".\downloading\update.zip"; // this is also a Path where the file saves but it only saves at the 4K Launcher rn 
-        string ServerVersionURL = ""; // use pastebin, type the current number as "001" not "0.0.1"
-        string ClientVersionPath = @"..\game\version.txt"; // same with the serverversionurl string but use a txt file instead
-        string GamePath = @"..\game";
-        string GameExecutable = @"..\game\game.exe"; // it can be also a steamid launch, a lnk file or whatever.
-        string GameName = "Game";
+        private readonly string DownloadURL = (""); // download url, requries a forced download
+        private readonly string UpdateZipFile = @".\downloading\update.zip"; // this is also a Path where the file saves but it only saves at the 4K Launcher rn 
+        private readonly string ServerVersionURL = ""; // use pastebin, type the current number as "001" not "0.0.1"
+        private readonly string ClientVersionPath = @"..\game\version.txt"; // same with the serverversionurl string but use a txt file instead
+        private readonly string GamePath = @"..\game";
+        private readonly string GameExecutable = @"..\game\game.exe"; // it can be also a steamid launch, a lnk file or whatever.
+        private readonly string GameName = "Game";
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e) 
+        private void Form1_Load(object sender, EventArgs e)
         {
             // TODO: Make a shared code function or smth from update_beans_Click
         }
@@ -49,21 +48,21 @@ namespace _4K_Launcher
             }
 
         }
-         private void update_beans_Click(object sender, EventArgs e)
-         {
-             WebClient client = new WebClient();
-             int Server = int.Parse(client.DownloadString(ServerVersionURL));
-             int ClientVersion = int.Parse(File.ReadAllText(ClientVersionPath));
-             if (Server > ClientVersion)
-             {
-                 client.DownloadFile(DownloadURL, ZipFile);
-                 try
-                 {
+        private void update_beans_Click(object sender, EventArgs e)
+        {
+            WebClient Client = new WebClient();
+            int Server = int.Parse(Client.DownloadString(ServerVersionURL));
+            int ClientVersion = int.Parse(File.ReadAllText(ClientVersionPath));
+            if (Server > ClientVersion)
+            {
+                Client.DownloadFile(DownloadURL, UpdateZipFile);
+                try
+                {
 
-                     {
-                            String ZipPath = ZipFile;
-                            String ExtractPath = GamePath;
-                            ZipFile.ExtractToDirectory(ZipPath, ExtractPath);
+                    {
+                        string ZipPath = UpdateZipFile;
+                        string ExtractPath = GamePath;
+                        ZipFile.ExtractToDirectory(ZipPath, ExtractPath);
                         if (MessageBox.Show(GameName + " has been successfully updated", "4K Game Launcher", MessageBoxButtons.OK, MessageBoxIcon.Information) == DialogResult.OK)
                         {
                             File.Delete(@".downloading\update.zip");
@@ -71,19 +70,19 @@ namespace _4K_Launcher
 
                     }
 
-                 }
-                 catch (Exception)
-                 {
-                     MessageBox.Show("Unable to download update or extract Zip file", "4K Game Launcher",
-                       MessageBoxButtons.OK, MessageBoxIcon.Error);
-                 }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unable to download update or extract Zip file", "4K Game Launcher",
+                      MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
 
-             }
-             else
-             {
-                 MessageBox.Show(GameName + "is already up-to-date", "4K Game Launcher",
-                      MessageBoxButtons.OK, MessageBoxIcon.Information);
-             } 
+            }
+            else
+            {
+                MessageBox.Show(GameName + "is already up-to-date", "4K Game Launcher",
+                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
     }
-}
 }
